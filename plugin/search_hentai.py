5c71547d9d987fd27414bdc701c4350da28bc6dd
Abhishek Sengupta <106603514+silverfruitplayer@users.noreply.github.com>
2025-08-09 14:06:39 +0530

    Update search_hentai.py

--- plugin/search_hentai.py
+++ plugin/search_hentai.py
@@ -5,14 +5,16 @@ import json
 from pyrogram.errors.exceptions.bad_request_400 import ButtonDataInvalid
 import sys
 
-def hentaisearch(client, message):
+
+@Client.on_message(filters.command("search"))
+async def hentaisearch(_, message):
     msg = message.text
     msgSplit = msg.split(" ")
     msgSplit.remove(msgSplit[0])
     query = " "
     query = query.join(msgSplit)
     if query == "":
-        client.send_animation(chat_id=message.chat.id,
+        client.send_animation(chat_id=message.chat_id,
                               animation="https://telegra.ph/file/cdeae50a8a23041b01935.mp4",
                               caption=f"""**/search <space> hentai name**""", parse_mode="markdown")
     else:
@@ -26,7 +28,7 @@ def hentaisearch(client, message):
             name = x["name"]
             keyb.append([InlineKeyboardButton(f"{name}", callback_data=f"info_{id}")])
         if keyb == []:
-            message.reply_text("No results found, Please check the Spelling and try again")
+            await message.reply_text("No results found, Please check the Spelling and try again")
         else:
             repl = InlineKeyboardMarkup(keyb)
-            message.reply_text(f"Your Search Results for **{query}**", reply_markup=repl, parse_mode="markdown")
+            await message.reply_text(f"Your Search Results for **{query}**", reply_markup=repl, parse_mode="markdown")
