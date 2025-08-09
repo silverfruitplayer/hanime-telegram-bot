652dd2ef102c359c4f6464177c126178dac1429f
Abhishek Sengupta <106603514+silverfruitplayer@users.noreply.github.com>
2025-08-09 12:55:06 +0530

    Update start.py

--- plugin/start.py
+++ plugin/start.py
@@ -1,5 +1,6 @@
 from pyrogram import *
 from pyrogram.types import *
 
-def start(client, message):
-    message.reply_text("""**/search <hentai name> to search**\n Powered By @MetaVoid""", parse_mode="markdown")
+@Client.on_message(filters.command("start"))
+async def start(client, message):
+   await message.reply_text("""**/search <hentai name> to search**\n Powered By @MetaVoid""")
