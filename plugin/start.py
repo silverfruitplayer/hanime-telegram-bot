be3c7e12fd0901ca833b593c85fe8e0b3366c9d4
Abhishek Sengupta <106603514+silverfruitplayer@users.noreply.github.com>
2025-08-13 15:46:28 +0530

    lol api ded

--- plugin/start.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import *
-from pyrogram.types import *
-
-@Client.on_message(filters.command("start"))
-async def start(client, message):
-   await message.reply_text("""**/search <hentai name> to search**\n Powered By @MetaVoid""")
